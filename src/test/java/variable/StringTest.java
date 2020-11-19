649ac13a4745b4955509acfd6a1ab3e7f6f69b0a
seondongpyo <psd1390@gmail.com>
2020-11-19 18:08:06 +0900

    문자열 (String) 내용 보충

--- src/test/java/variable/StringTest.java
+++ src/test/java/variable/StringTest.java
@@ -8,24 +8,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StringTest {
 
     @Test
-    @DisplayName("문자열 + '아무 타입' = 문자열")
-    void stringPlusAnyTypeIsString() {
+    @DisplayName("문자열 + '기본형 타입' = 문자열")
+    void casePrimitiveTypeToStringByPlusString() {
+        char charValue = 'A';
         byte byteValue = 10;
         short shortValue = 20;
         int intValue = 30;
         long longValue = 40;
         float floatValue = 3.14f;
-        double doubleValue = 11.19;
-        char charValue = 'A';
+        double doubleValue = 11.19d;
         String stringValue = "String";
 
-        assertThat(stringValue + byteValue).isInstanceOf(String.class);
-        assertThat(stringValue + shortValue).isInstanceOf(String.class);
-        assertThat(stringValue + intValue).isInstanceOf(String.class);
-        assertThat(stringValue + longValue).isInstanceOf(String.class);
-        assertThat(stringValue + floatValue).isInstanceOf(String.class);
-        assertThat(stringValue + doubleValue).isInstanceOf(String.class);
-        assertThat(stringValue + charValue).isInstanceOf(String.class);
+        assertThat(stringValue + charValue).isEqualTo("StringA").isInstanceOf(String.class);
+        assertThat(stringValue + byteValue).isEqualTo("String10").isInstanceOf(String.class);
+        assertThat(stringValue + shortValue).isEqualTo("String20").isInstanceOf(String.class);
+        assertThat(stringValue + intValue).isEqualTo("String30").isInstanceOf(String.class);
+        assertThat(stringValue + longValue).isEqualTo("String40").isInstanceOf(String.class);
+
+        // <주의> 자료형의 접미사는 문자열로 변환되지 않는다
+        assertThat(stringValue + floatValue).isEqualTo("String3.14").isInstanceOf(String.class);
+        assertThat(stringValue + doubleValue).isEqualTo("String11.19").isInstanceOf(String.class);
+    }
+
+    @Test
+    @DisplayName("String.valueOf()를 이용하여 기본형 타입을 문자열로 변환하기")
+    void castPrimitiveTypeToStringByValueOf() {
+        char charValue = 'A';
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 30;
+        long longValue = 40;
+        float floatValue = 3.14f;
+        double doubleValue = 11.19d;
+
+        assertThat(String.valueOf(charValue)).isEqualTo("A").isInstanceOf(String.class);
+        assertThat(String.valueOf(byteValue)).isEqualTo("10").isInstanceOf(String.class);
+        assertThat(String.valueOf(shortValue)).isEqualTo("20").isInstanceOf(String.class);
+        assertThat(String.valueOf(intValue)).isEqualTo("30").isInstanceOf(String.class);
+        assertThat(String.valueOf(longValue)).isEqualTo("40").isInstanceOf(String.class);
+
+        // <주의> 자료형의 접미사는 문자열로 변환되지 않는다
+        assertThat(String.valueOf(floatValue)).isEqualTo("3.14").isInstanceOf(String.class);
+        assertThat(String.valueOf(doubleValue)).isEqualTo("11.19").isInstanceOf(String.class);
     }
 
     @Test
