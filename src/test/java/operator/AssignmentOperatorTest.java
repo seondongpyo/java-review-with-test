8fa16eb940e956278a2c55728966d606e6282d96
seondongpyo <psd1390@gmail.com>
2020-11-23 11:38:19 +0900

    비트 대입 연산자 추가

--- src/test/java/operator/AssignmentOperatorTest.java
+++ src/test/java/operator/AssignmentOperatorTest.java
@@ -10,15 +10,23 @@ class AssignmentOperatorTest {
     @Test
     @DisplayName("대입 연산자")
     void assignmentOperator() {
-        int intValue = 10;
+        int value1 = 10;
 
-        assertThat(intValue).isEqualTo(10);
-        assertThat(intValue += 10).isEqualTo(20);
-        assertThat(intValue -= 5).isEqualTo(15);
-        assertThat(intValue *= 10).isEqualTo(150);
-        assertThat(intValue /= 5).isEqualTo(30);
-        assertThat(intValue %= 4).isEqualTo(2);
-        assertThat(intValue).isEqualTo(2);
+        assertThat(value1).isEqualTo(10);
+        assertThat(value1 += 10).isEqualTo(20);
+        assertThat(value1 -= 5).isEqualTo(15);
+        assertThat(value1 *= 10).isEqualTo(150);
+        assertThat(value1 /= 5).isEqualTo(30);
+        assertThat(value1 %= 4).isEqualTo(2);
+        assertThat(value1).isEqualTo(2);
+
+        int value2 = 8;
+        assertThat(value2 <<= 2).isEqualTo(32); // 0000 1000 << 2 = 0010 0000
+        assertThat(value2 >>= 3).isEqualTo(4); // 0010 0000 >> 3 = 0000 0100
+        assertThat(value2 &= 7).isEqualTo(4); // 0100 & 0111 = 0100
+        assertThat(value2 |= 11).isEqualTo(15);  // 0100 | 1011 = 1111
+        assertThat(value2 ^= 2).isEqualTo(13); // 1111 ^ 0010 = 1101
+        assertThat(value2 >>>= 2).isEqualTo(3); // 1101 >>> 2 = 0011
     }
 
 }
