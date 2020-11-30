28ce817338c9e256f85944da79909bca1b877bdd
seondongpyo <psd1390@gmail.com>
2020-11-30 10:56:06 +0900

    클래스 메서드와 인스턴스 메서드 (내용 보충)

--- src/test/java/method/MethodTest.java
+++ src/test/java/method/MethodTest.java
@@ -62,6 +62,8 @@ class MethodTest {
             - 인스턴스 변수나 인스턴스 메서드와 관련 없는 작업을 하는 메서드
             - 메서드 내에서 인스턴스 변수는 사용할 수 없음
             - 메서드 내에서 인스턴스 변수를 사용하지 않는다면 static 키워드를 붙이는 것을 고려한다
+                => 메서드 호출 시간이 짧아지기 때문에 효율이 높아짐
+                   인스턴스 메서드는 호출 시 메서드를 찾는 과정이 추가적으로 필요하기 때문에 시간이 더 걸림
          */
         int addByClassMethod = Calculator.add(20, 30);
         assertThat(addByClassMethod).isEqualTo(50);
@@ -75,6 +77,13 @@ class MethodTest {
         Calculator calc = new Calculator();
         int addByInstanceMethod = calc.add();
         assertThat(addByInstanceMethod).isEqualTo(30);
+
+        // Q. 인스턴스를 생성한 후 클래스 메서드를 호출하게 되면?
+        //  => 인스턴스가 생성될 때 메모리의 힙 영역에 저장되는데, 인스턴스를 생성하려면 클래스를 먼저 로드해야 되고
+        //     이 때 클래스 멤버들은 메서드 영역에 저장되어 있기 때문에 추가적인 메모리 사용이 불필요하기 때문
+        //     또한 다른 사용자가 코드를 봤을 때 클래스 메서드가 아닌 인스턴스 메서드로 오해할 수 있음
+        int add = calc.add(10, 20); // 정상적으로 실행은 되나, 권장하지 않음
+        assertThat(add).isEqualTo(30);
     }
 
     private int add(int a, int b) {
@@ -110,5 +119,35 @@ class Calculator {
     // 클래스 메서드
     static int add(int x, int y) {
         return x + y;
+    }
+}
+
+class TestClass {
+
+    int localVariable;
+    static int classVariable;
+
+    void instanceMethod1() {}
+    static void staticMethod1() {}
+
+    void instanceMethod2() {
+        classVariable = 20; // 클래스 변수 사용 가능
+        staticMethod1(); // static 메서드 호출 가능
+
+        localVariable = 10; // 인스턴스 변수 사용 가능
+        instanceMethod1(); // 인스턴스 메서드 호출 가능
+    }
+
+    static void staticMethod2() {
+        classVariable = 30; // 클래스 변수 사용 가능
+        staticMethod1(); // static 메서드 호출 가능
+
+//        localVariable = 10; // 에러
+//        instanceMethod2(); // 에러
+        // => 클래스 멤버들은 인스턴스를 생성하지 않고도 사용할 수 있는데,
+        //    클래스 메서드를 호출하는 경우, 호출 시점에 인스턴스가 생성되어 있을 수도, 또는 없을 수도 있다
+        //    따라서, 인스턴스를 생성해야만 사용할 수 있는 인스턴스 멤버들은 사용할 수 없다
+        //    반대로 인스턴스 메서드에서 클래스 멤버를 사용하고자 하는 경우,
+        //    인스턴스 멤버가 존재하는 시점에 이미 클래스 멤버가 메모리에 존재하기 때문에 사용할 수 있다
     }
 }
