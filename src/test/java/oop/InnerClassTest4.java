525c30fc028515d546543bfd131f5f5afdba5bd1
Seondong Pyo <psd1390@gmail.com>
2020-12-09 23:25:45 +0900

    내부 클래스의 제어자와 접근성 (this)

---
+++ src/test/java/oop/InnerClassTest4.java
@@ -0,0 +1,42 @@
+package oop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InnerClassTest4 {
+
+    @Test
+    @DisplayName("내부 클래스의 제어자와 접근성 3")
+    void innerClassModifier() {
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner();
+
+        assertThat(inner.method()).isEqualTo(30);
+        assertThat(inner.thisMethod()).isEqualTo(20);
+        assertThat(inner.outerThisMethod()).isEqualTo(10);
+    }
+
+    class Outer {
+        int value = 10; // Outer.this.value
+
+        class Inner {
+            int value = 20; // this.value
+
+            int method() {
+                int value = 30;
+                return value;
+            }
+
+            int thisMethod() {
+                return this.value;
+            }
+
+            int outerThisMethod() {
+                return Outer.this.value;
+            }
+        }
+    }
+}
