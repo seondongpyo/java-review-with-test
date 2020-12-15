605da0d9f3714306db403c1ee786de0b113dd9db
Seondong Pyo <psd1390@gmail.com>
2020-12-15 23:25:05 +0900

    package import 정리

--- src/test/java/classes/StringTest.java
+++ src/test/java/classes/StringTest.java
@@ -1,13 +1,9 @@
 package classes;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.StringJoiner;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringTest {
--- src/test/java/oop/PolymorphismTest.java
+++ src/test/java/oop/PolymorphismTest.java
@@ -3,10 +3,7 @@ package oop;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.awt.image.BufferedImage;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 class PolymorphismTest {
 
