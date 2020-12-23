a5a01d1226a06bcb311c0e43ab0e48f2402b0fe1
Seondong Pyo <psd1390@gmail.com>
2020-12-23 22:03:28 +0900

    Map 인터페이스와 Iterator

--- src/test/java/collection/IteratorTest.java
+++ src/test/java/collection/IteratorTest.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class IteratorTest {
@@ -45,5 +43,27 @@ class IteratorTest {
         assertThrows(NoSuchElementException.class, () -> {
             iterator.next(); // 읽어올 다음 요소가 없으면 NoSuchElementException 예외 발생
         });
+    }
+
+    @Test
+    @DisplayName("Map 인터페이스와 Iterator")
+    void mapIterator() {
+        Map<Integer, Object> map = new HashMap<>();
+        map.put(1, "홍길동");
+        map.put(2, "김길동");
+
+        // Map 인터페이스를 구현한 컬렉션 클래스는 key와 value를 하나의 쌍으로 가지기 때문에 iterator()를 직접 호출할 수는 없다
+        // 대신 keySet() 또는 entrySet()과 같이 key와 value를 각각 따로 Set의 형태로 얻어온 후 iterator()를 호출할 수 있다
+
+        Set<Integer> keySet = map.keySet(); // Map에 들어있는 모든 키들을 반환
+        Collection<Object> values = map.values(); // Map에 들어있는 모든 값들을 반환
+
+        Iterator<Integer> keyIterator = keySet.iterator();
+        Iterator<Object> valueIterator = values.iterator();
+
+        assertThat(keyIterator.next()).isEqualTo(1);
+        assertThat(keyIterator.next()).isEqualTo(2);
+        assertThat(valueIterator.next()).isEqualTo("홍길동");
+        assertThat(valueIterator.next()).isEqualTo("김길동");
     }
 }
