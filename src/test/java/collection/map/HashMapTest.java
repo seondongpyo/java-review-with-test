fe89bb5684ac5c88d63ddd77b684668923ee016a
seondongpyo <psd1390@gmail.com>
2020-12-30 20:03:11 +0900

    HashMap 클래스 put(), putAll() - Map에 요소를 저장

---
+++ src/test/java/collection/map/HashMapTest.java
@@ -0,0 +1,51 @@
+package collection.map;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HashMapTest {
+
+    /*
+        << HashMap >>
+        - Map 인터페이스를 구현한 클래스로, 키(key)와 값(value)을 묶어서 하나의 데이터(entry)로 저장한다
+        - 해싱(hashing)을 사용하기 때문에, 많은 양의 데이터를 검색하는 데 있어서 성능이 뛰어나다
+     */
+
+    @Test
+    @DisplayName("HashMap 클래스 put() - 지정된 키와 값을 저장")
+    void put() {
+        HashMap<Integer, String> users = new HashMap<>();
+        users.put(1, "김길동");
+        users.put(2, "이길동");
+        users.put(3, "최길동");
+        users.put(4, "홍길동");
+        users.put(4, "박길동"); // 키는 중복을 허용하지 않음. 기존 키(4)에 대한 값이 '홍길동'에서 '박길동'으로 변경됨
+        users.put(5, "박길동"); // 값은 중복을 허용함
+
+        assertThat(users).hasSize(5);
+        assertThat(users).containsValues("김길동", "이길동", "최길동", "박길동", "박길동");
+        assertThat(users).doesNotContainValue("홍길동");
+    }
+    
+    @Test
+    @DisplayName("HashMap 클래스 putAll() - 전달한 Map에 저장된 모든 요소를 저장")
+    void putAll() {
+        HashMap<Integer, String> users = new HashMap<>();
+        users.put(1, "김길동");
+        users.put(2, "이길동");
+        users.put(3, "최길동");
+        users.put(4, "홍길동");
+
+        HashMap<Integer, String> newUsers = new HashMap<>();
+        newUsers.putAll(users); // users에 저장된 모든 요소를 newUsers에 저장, 'new HashMap<>(users)'와 동일함
+        newUsers.put(5, "박길동");
+
+        assertThat(newUsers).hasSize(5);
+        assertThat(newUsers).containsValues("김길동", "이길동", "최길동", "홍길동", "박길동");
+    }
+}
