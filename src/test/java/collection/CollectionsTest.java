6c86f68a4de47b67363763b5422edec77afa787b
seondongpyo <psd1390@gmail.com>
2021-01-01 19:17:39 +0900

    Collections 클래스 - 단일 컬렉션

--- src/test/java/collection/CollectionsTest.java
+++ src/test/java/collection/CollectionsTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CollectionsTest {
@@ -29,6 +30,19 @@ class CollectionsTest {
 
         assertThrows(UnsupportedOperationException.class, () -> {
             singletonList.add("another string"); // 싱글톤 컬렉션에 요소를 저장하려고 할 경우
+        });
+    }
+
+    @Test
+    @DisplayName("Collections 클래스 - 단일(한 종류의 객체만 저장하는) 컬렉션 만들기")
+    void checkedList() {
+        List checkedList = Collections.checkedList(new ArrayList<>(), Integer.class);
+        checkedList.add(10);
+        checkedList.add(new Integer(20));
+
+        assertThat(checkedList).hasSize(2);
+        assertThrows(ClassCastException.class, () -> {
+            checkedList.add(3.14f); // int 대신 float 자료형 요소를 저장하려고 할 경우
         });
     }
 }
