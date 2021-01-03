e9d013b3715cdaf2bf7474e1d6022e331e8578ca
seondongpyo <psd1390@gmail.com>
2021-01-03 12:56:06 +0900

    열거형

---
+++ src/test/java/enumtest/EnumTest.java
@@ -0,0 +1,83 @@
+package enumtest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EnumTest {
+
+    @Test
+    @DisplayName("열거형(enum)의 정의와 사용")
+    void enumType() {
+        Unit unit = new Unit();
+        unit.faceEast();
+
+        assertThat(unit.direction == Direction.EAST).isTrue(); // 열거형은 '==' 비교 가능
+    }
+
+    @Test
+    @DisplayName("열거형 메서드 name() - 열거형 상수의 이름을 문자열로 반환")
+    void name() {
+        String east = Direction.EAST.name();
+        String west = Direction.WEST.name();
+        String south = Direction.SOUTH.name();
+        String north = Direction.NORTH.name();
+
+        assertThat(east).isEqualTo("EAST");
+        assertThat(west).isEqualTo("WEST");
+        assertThat(south).isEqualTo("SOUTH");
+        assertThat(north).isEqualTo("NORTH");
+    }
+
+    @Test
+    @DisplayName("열거형 메서드 ordinal() - 열거형 상수가 정의된 순서를 반환 (0부터 시작)")
+    void ordinal() {
+        int east = Direction.EAST.ordinal();
+        int west = Direction.WEST.ordinal();
+        int south = Direction.SOUTH.ordinal();
+        int north = Direction.NORTH.ordinal();
+
+        assertThat(east).isEqualTo(0);
+        assertThat(west).isEqualTo(1);
+        assertThat(south).isEqualTo(2);
+        assertThat(north).isEqualTo(3);
+    }
+    
+    @Test
+    @DisplayName("열거형 메서드 valueOf() - 지정된 열거형에서 name과 일치하는 열거형 상수를 반환")
+    void valueOf() {
+        Direction east = Enum.valueOf(Direction.class, "EAST");
+        Direction west = Enum.valueOf(Direction.class, "WEST");
+        Direction south = Enum.valueOf(Direction.class, "SOUTH");
+        Direction north = Enum.valueOf(Direction.class, "NORTH");
+
+        assertThat(east).isEqualTo(Direction.EAST);
+        assertThat(west).isEqualTo(Direction.WEST);
+        assertThat(south).isEqualTo(Direction.SOUTH);
+        assertThat(north).isEqualTo(Direction.NORTH);
+    }
+    
+    @Test
+    @DisplayName("열거형 메서드 values() - 열거형에 정의된 모든 상수를 정의된 순서대로 반환")
+    void values() {
+        Direction[] directions = Direction.values();
+
+        assertThat(directions[0]).isEqualTo(Direction.EAST);
+        assertThat(directions[1]).isEqualTo(Direction.WEST);
+        assertThat(directions[2]).isEqualTo(Direction.SOUTH);
+        assertThat(directions[3]).isEqualTo(Direction.NORTH);
+    }
+
+    static class Unit {
+        Direction direction;
+
+        void faceEast() {
+            direction = Direction.EAST;
+        }
+    }
+
+    enum Direction {
+        EAST, WEST, SOUTH, NORTH
+    }
+}
