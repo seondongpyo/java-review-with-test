f037a73d3fd546644c2de3095ba3ec3bdb128856
seondongpyo <psd1390@gmail.com>
2021-01-13 21:57:45 +0900

    java.util.fuction 패키지 - Predicate<T>

--- src/test/java/lambda/FunctionTest.java
+++ src/test/java/lambda/FunctionTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,5 +36,31 @@ class FunctionTest {
 
         assertThat(parseStringToInteger.apply("100")).isEqualTo(100);
         assertThat(doubleString.apply("test")).isEqualTo("testtest");
+    }
+
+    @Test
+    @DisplayName("java.util.function 패키지 - Predicate<T>")
+    void predicate() {
+        /*
+            << Predicate<T> >>
+            - 조건식을 표헌하는 데 사용됨
+            - 매개변숳는 하나이며, 반환 타입은 boolean
+         */
+
+        /*
+        // 문자열이 비어있는지 확인하는 함수
+        Predicate<String> isEmptyStr = new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                return s.length() == 0;
+            }
+        };
+         */
+        Predicate<String> isEmptyString = s -> s.length() == 0; // 람다식으로 간소화
+        String str1 = "";
+        String str2 = "test";
+
+        assertThat(isEmptyString.test(str1)).isTrue();
+        assertThat(isEmptyString.test(str2)).isFalse();
     }
 }
