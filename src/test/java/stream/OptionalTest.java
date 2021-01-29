047e121f886e493adf4e58b2e744ce6bf7bf7263
seondongpyo <psd1390@gmail.com>
2021-01-29 22:20:39 +0900

    Optional<T> 객체의 값 가져오기

--- src/test/java/stream/OptionalTest.java
+++ src/test/java/stream/OptionalTest.java
@@ -44,11 +44,77 @@ public class OptionalTest {
         assertThat(emptyOptional).isEmpty();
     }
 
+    @Test
+    @DisplayName("Optional<T> 객체의 값 가져오기 - get")
+    void optional_get() {
+        Optional<String> hello = Optional.of("hello");
+
+        assertThat(hello.get()).isEqualTo("hello");
+    }
+
+    @Test
+    @DisplayName("Optional<T> 객체의 값 가져오기 - orElse")
+    void optional_orElse() {
+        // orElse(String other) - 참조변수에 저장된 값이 null일 경우 대체할 값을 지정할 수 있다
+        String hello = getOptionalOrElse("hello");
+        String orElse = getOptionalOrElse(null);
+
+        assertThat(hello).isEqualTo("hello");
+        assertThat(orElse).isEqualTo("This is null");
+    }
+
+    @Test
+    @DisplayName("Optional<T> 객체의 값 가져오기 - orElseGet")
+    void optional_orElseGet() {
+        // orElseGet(Supplier<? extends T> other) - null을 대체할 값을 반환하는 람다식을 지정할 수 있다
+        String hello = getOptionalOrElseGet("hello");
+        String orElseGet = getOptionalOrElseGet(null);
+
+        assertThat(hello).isEqualTo("hello");
+        assertThat(orElseGet).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName("Optional<T> 객체의 값 가져오기 - orElseThrow")
+    void optional_orElseThrow() {
+        // orElseThrow(Supplier<? extends X> exceptionSupplier) - null일 때 지정된 예외를 발생시킨다
+        String hello = getOptionalOrElseThrow("hello");
+        assertThat(hello).isEqualTo("hello");
+
+        assertThrows(NullPointerException.class, () -> {
+            getOptionalOrElseThrow(null);
+        });
+    }
+
+    @Test
+    @DisplayName("Optional<T> 객체의 값 가져오기 - isPresent")
+    void optional_isPresent() {
+        // isPresent() - Optional<T> 객체의 값이 null이면 false를, 값이 있으면 true를 반환한다
+        assertThat(getOptionalIsPresent("hello")).isTrue();
+        assertThat(getOptionalIsPresent(null)).isFalse();
+    }
+
     public Optional<String> getOptionalOf(String value) {
         return Optional.of(value);
     }
 
     public Optional<String> getOptionalOfNullable(String value) {
         return Optional.ofNullable(value);
+    }
+
+    public String getOptionalOrElse(String value) {
+        return Optional.ofNullable(value).orElse("This is null");
+    }
+
+    public String getOptionalOrElseGet(String value) {
+        return Optional.ofNullable(value).orElseGet(String::new);
+    }
+
+    public String getOptionalOrElseThrow(String value) {
+        return Optional.ofNullable(value).orElseThrow(NullPointerException::new);
+    }
+
+    public boolean getOptionalIsPresent(String value) {
+        return Optional.ofNullable(value).isPresent();
     }
 }
